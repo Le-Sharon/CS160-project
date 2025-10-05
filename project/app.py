b20c68591d04a363804c307d72f044d23733f976
Sharon Le <sharon.le@sjsu.edu>
2025-10-05 02:21:30 -0700

    update
    added basic layout of project

---
+++ project/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import io, csv, os, json, math
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/importCSV', methods=['POST'])
+def import_csv():
+    return 0
+
+@app.route('/getLayer', methods=['GET'])
+def get_layer():
+    return 0
+
+@app.route('/getBuffer', methods=['POST'])
+def get_buffer():
+    return 0
+    
+@app.route('/compareLayers', methods=['POST'])
+def compare_layers():
+    return 0
+
+
+@app.route('/exportCSV', methods=['GET'])
+def export_csv():
+    return 0
