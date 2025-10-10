f9fa6222422977c286ef31671ded12b9b22c2281
Sharon Le <sharon.le@sjsu.edu>
2025-10-10 10:57:00 -0700

    added leaflet map

---
+++ project/static/js/main.js
@@ -0,0 +1,19 @@
+// Initialize maps
+let map = L.map('map').setView([37.3387, -121.8853], 13);
+let csvMap = L.map('csvMap').setView([37.3387, -121.8853], 13);
+let compareMap = L.map('compareMap').setView([37.3387, -121.8853], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(csvMap);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(compareMap);
